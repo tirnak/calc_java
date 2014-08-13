9586c5d4c07de2e9259b2afa6c14d757bb708edb
Kirill <semenovkirill.spb@gmail.com>
2014-08-13 17:59:04 +0400

    trying to RPN

--- src/com/example/kirill/CalcToken.java
+++ src/com/example/kirill/CalcToken.java
@@ -6,47 +6,110 @@ package com.example.kirill;
 public class CalcToken {
 
     public enum Type {
+        NULL,
         NUM,
         FUNC,
         ADD,
+        SUBSTRACT,
         MULTIPLY,
         DIVIDE,
         OPEN_BRACKET,
         CLOSE_BRACKET
     }
 
+    public enum Group {
+        NULL,
+
+    }
+
     public String content = new String();
+
+    public Type type;
 
     public CalcToken(String str) {
         content = str;
+
+        if (isInteger()) {
+            type = Type.NUM;
+        } else if (isAddition()) {
+            type = Type.ADD;
+        } else if (isFunc()) {
+            type = Type.FUNC;
+        } else if (isSubstraction()) {
+            type = Type.SUBSTRACT;
+        } else if (isMultiplication()) {
+            type = Type.MULTIPLY;
+        } else if (isDivision()) {
+            type = Type.DIVIDE;
+        } else if (isOpenBracket()) {
+            type = Type.OPEN_BRACKET;
+        } else if (isClosedBracket()) {
+            type = Type.CLOSE_BRACKET;
+        } else {
+            type = Type.NULL;
+        }
     }
 
     public Type getType() {
-        return Type.OPEN_BRACKET;
+        return type;
     }
 
-    private boolean isInteger(String str) {
-        if (str == null) {
+
+    public boolean isStartOfSubExpr () {
+        switch (getType()) {
+            case OPEN_BRACKET:
+            case NULL:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean isInteger() {
+
+        if (content == "PI" || content == "E") {
+            return true;
+        }
+
+        try {
+            Double.parseDouble(content);
+        } catch(Exception e) {
             return false;
         }
-        int length = str.length();
-        if (length == 0) {
-            return false;
-        }
-        int i = 0;
-        if (str.charAt(0) == '-') {
-            if (length == 1) {
-                return false;
-            }
-            i = 1;
-        }
-        for (; i < length; i++) {
-            char c = str.charAt(i);
-            if (c <= '/' || c >= ':') {
-                return false;
-            }
-        }
+
         return true;
     }
+
+    private boolean isFunc() {
+        return content == "cos" || content == "sin" || content == "exp";
+    }
+
+    private boolean isAddition() {
+        return content == "+";
+    }
+
+    private boolean isSubstraction() {
+        return content == "-";
+    }
+
+    private boolean isMultiplication() {
+        return content == "*";
+    }
+
+    private boolean isDivision() {
+        return content == "/";
+    }
+
+    private boolean isOpenBracket() {
+        return content == "(";
+    }
+
+    private boolean isClosedBracket() {
+        return content == ")";
+    }
+
+
+
+
 
 }
--- src/com/example/kirill/Parser.java
+++ src/com/example/kirill/Parser.java
@@ -1,30 +1,141 @@
 package com.example.kirill;
 
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Parser {
 
     public LinkedList<CalcToken> tokens = new LinkedList<CalcToken>();
 
     public Parser(String str) {
+
+        tokens.push(new CalcToken(""));
+
         StringTokenizer st = new StringTokenizer(str, "+-*/()", true);
 
         while (st.hasMoreTokens()) {
-            tokens.add(new CalcToken(st.nextToken()));
-            System.out.println(tokens.peekLast().content);
+            String tString = st.nextToken();
+
+            if (tString == "-" && tokens.peekLast().isStartOfSubExpr()) {
+
+            }
+
+            tokens.add(new CalcToken(tString));
+        }
+
+        tokens.add(new CalcToken(""));
+
+        correctNegativeNumbers();
+
+        for (CalcToken token: tokens) {
+            System.out.println(
+                    token.content
+            );
         }
     }
 
-    public LinkedList<String> getTokensInRPNotation() {
+    public LinkedList<CalcToken> getTokensInRPNotation() {
 
-        LinkedList<String> rpnTokens = new LinkedList<String>();
-        LinkedList<String> tmpStack = new LinkedList<String>();
+        LinkedList<CalcToken> rpnTokens = new LinkedList<CalcToken>();
+        LinkedList<CalcToken> tmpStack = new LinkedList<CalcToken>();
 
-        for (CalcToken token : tokens) {
+        tmpStack.push(
+                tokens.pop()
+        );
 
-        }
+        Mapper.Action action;
+
+        do {
+            action = Mapper.map(tokens.peek(), tmpStack.peek());
+
+            switch (action) {
+                case MOVE_TO_TMP_STACK:
+                    break;
+                case MOVE_TO_RPN_STACK:
+                    break;
+                case ERASE_BOTH_TOKENS:
+                    break;
+                case RETURN_ERROR:
+                    break;
+            }
+        } while (action != Mapper.Action.FINISH);
 
         return rpnTokens;
+    }
+
+    private static class Mapper {
+        public enum Action {
+            MOVE_TO_TMP_STACK,
+            MOVE_TO_RPN_STACK,
+            ERASE_BOTH_TOKENS,
+            FINISH,
+            RETURN_ERROR
+        }
+
+        public static Action map(CalcToken fromInfix, CalcToken fromTmpStack) {
+
+            switch (fromInfix.getType()) {
+
+                case OPEN_BRACKET:
+                    return Action.MOVE_TO_TMP_STACK;
+                case NULL:
+                    if (fromTmpStack.getType() == CalcToken.Type.NULL) {
+                        return Action.FINISH;
+                    } else if (fromTmpStack.getType() == CalcToken.Type.OPEN_BRACKET) {
+                        return Action.RETURN_ERROR;
+                    } else {
+                        return Action.MOVE_TO_RPN_STACK;
+                    }
+                case ADD:
+                case SUBSTRACT:
+                case NUM:
+                    if (fromTmpStack.getType() == CalcToken.Type.NULL || fromTmpStack.getType() == CalcToken.Type.OPEN_BRACKET) {
+                        return Action.MOVE_TO_TMP_STACK;
+                    } else {
+                        return Action.MOVE_TO_RPN_STACK;
+                    }
+                case MULTIPLY:
+                case DIVIDE:
+                    if (fromTmpStack.getType() == CalcToken.Type.MULTIPLY || fromTmpStack.getType() == CalcToken.Type.DIVIDE) {
+                        return Action.MOVE_TO_RPN_STACK;
+                    } else {
+                        return Action.MOVE_TO_TMP_STACK;
+                    }
+                case CLOSE_BRACKET:
+                    if (fromTmpStack.getType() == CalcToken.Type.NULL) {
+                        return Action.RETURN_ERROR;
+                    } else if (fromTmpStack.getType() == CalcToken.Type.OPEN_BRACKET) {
+                        return Action.ERASE_BOTH_TOKENS;
+                    } else {
+                        return Action.MOVE_TO_RPN_STACK;
+                    }
+            }
+
+            return Action.MOVE_TO_RPN_STACK;
+
+        }
+    }
+
+    private void correctNegativeNumbers() {
+        LinkedList<Integer> indexesToRemove = new LinkedList<Integer>();
+
+        for (int i = 0, length = tokens.size(); i < length; i++) {
+
+            if (tokens.get(i).getType() == CalcToken.Type.SUBSTRACT &&
+                    tokens.get(i-1).isStartOfSubExpr()) {
+
+                tokens.set(
+                        i+1,
+                        new CalcToken("-" + tokens.get(i+1))
+                );
+
+                indexesToRemove.push(i);
+            }
+        }
+
+        while (indexesToRemove.size() > 0) {
+            tokens.remove(
+                    indexesToRemove.pop()
+            );
+        }
     }
 }
